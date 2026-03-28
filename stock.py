1d076663a4aac6dc92a21909c49f967abe9cb0b4
Bakedwei <jayhsu3e@gmail.com>
2026-03-28 14:35:45 +0800

    輸出改善，小錯修理

--- stock.py
+++ stock.py
@@ -1,13 +1,11 @@
 import yfinance as yf
 import pandas as pd
+from company import get_stock
 
 chosenstock = []
-cpystock = []
-prestockls = ["2330.TW", "2388.TW", "2454.TW", "1101.TW", "1216.TW", "6505.TW", "2207.TW",
-               "2201.TW", "2548.TW", "2603.TW", "2707.TW", "2912.TW", "2731.TW", "9917.TW"]
-for i in range(0, len(prestockls)):
-    cpystock.append(prestockls[i])
-    chosenstock.append(yf.Ticker(prestockls[i]))
+stocks = []
+chosenstock, stocks = get_stock()
+
 
 def YangSwallow(data):
     data["Price_Difference"] = data["Open"] - data["Close"].shift(1)
@@ -35,25 +33,30 @@ def InSwallow(data):
     else:
         print("這支股票在過去兩星期內有以下交易日符合條件(陰吞)：")
         print(filtered_data[["Open", "Close"]])
-
-def threesolderBlack(data) :
+def threesolderBlack(data):
     data["Price_Lower"] = data["Close"] - data["Close"].shift(1)
     data["Price_Lower2"] = data["Close"].shift(1) - data["Close"].shift(2)
+    data["Price_Lower3"] = data["Close"].shift(2) - data["Close"].shift(3)
     data["Price_Lowera"] = data["Open"] - data["Open"].shift(1)
     data["Price_Lowera2"] = data["Open"].shift(1) - data["Open"].shift(2)
-    filtered_data = data[(data["Price_Lower"] > 0) &
-                          (data["Price_Lower2"] > 0) &
-                            (data["Price_Lowera"] >0) &
-                              (data["Price_Lowera2"]>0) &
-                              ((data["Close"].shift(2)-data["Open"].shift(2)) < 0) &
-                              ((data["Close"].shift(1)-data["Open"].shift(1)) < 0) &
-                              ((data["Close"]-data["Open"]) < 0)]
+    data["Price_Lowera3"] = data["Open"].shift(2) - data["Open"].shift(3)
+    
+    filtered_data = data[(data["Price_Lower"] < 0) &
+                         (data["Price_Lower2"] < 0) &
+                         (data["Price_Lower3"] < 0) &
+                         (data["Price_Lowera"] < 0) &
+                         (data["Price_Lowera2"] < 0) &
+                         (data["Price_Lowera3"] < 0) &
+                         ((data["Close"] - data["Open"]) < 0) &
+                         ((data["Close"].shift(1) - data["Open"].shift(1)) < 0) &
+                         ((data["Close"].shift(2) - data["Open"].shift(2)) < 0)]
 
     if filtered_data.empty:
         print("這支股票在過去兩星期內沒有符合條件的交易日(黑三兵)。")
     else:
         print("這支股票在過去兩星期內有以下交易日符合條件(黑三兵)：")
         print(filtered_data[["Open", "Close"]])
+
 
 def threesolderRed(data) :
     data["Price_Lower"] = data["Close"] - data["Close"].shift(1)
@@ -73,34 +76,31 @@ def threesolderRed(data) :
     else:
         print("這支股票在過去兩星期內有以下交易日符合條件(紅三兵)：")
         print(filtered_data[["Open", "Close"]])
-def create_ticker():
-    symbol = input("請輸入您想額外檢測的股票代碼: ")
+def create_ticker(symbol):
     ticker = yf.Ticker(symbol)
     return ticker
 
-print("你好呀!這裡是股票檢測小幫手，我們會預先幫你間檢測精選台股喔!\n這邊還可以加多支自選股喔!\n")
+print("你好呀!這裡是股票檢測小幫手，我們會預先幫你檢測台股喔!\n這邊還可以加多支自選股喔!\n")
 print("你想加幾支呢?")
 indexn = int(input())
-for i in range(0, indexn):
-    tmp= create_ticker()
-    chosenstock.append(tmp)
-
+if indexn != 0:
+    for i in range(0, indexn):
+        symbol = input("請輸入您想額外檢測的股票代碼: ")
+        stocks.append(symbol)
+        tmp = create_ticker(symbol)
+        chosenstock.append(tmp)
 for i in range(0, len(chosenstock)) :
-    cpystock.append(chosenstock[i])
     chosenstock[i] = chosenstock[i].history(period="2mo")
-
 for i in range(0, len(chosenstock)) :
     stock = chosenstock[i]
     if stock.empty:
         print("沒有這支股票捏;w;，再找找別的八\n")
     else:
         print("目前檢測股票為:")
-        print(cpystock[i])
+        print(stocks[i])
         #print("\n")
         YangSwallow(stock)
         InSwallow(stock)
         threesolderBlack(stock)
         threesolderRed(stock)
         print("\n")
-
-
